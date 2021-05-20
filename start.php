a0a8b035685dac4fa9438535cf370c5797770fe7
joeanyjos <63514730+joeanyjos@users.noreply.github.com>
2021-05-20 12:03:03 +0200

    Update start.php

--- start.php
+++ start.php
@@ -1,12 +1,14 @@
 <!DOCTYPE HTML>
 <?php
-
+//kijkt of username & password goed zijn
 if (isset($_POST['submit'])){
     $un=$_POST['username'];
     $pw=$_POST['password'];
+    //if username + password kloppen, redirect 
     if ($un=='username' && $pw=='password'){
         header("location:colors.html");
         exit();
+        //else echo alert
     } else {
         echo '<script language="javascript">';
         echo 'alert("Incorrect username or password")';
